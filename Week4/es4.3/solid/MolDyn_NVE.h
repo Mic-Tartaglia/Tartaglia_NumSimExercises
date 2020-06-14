96bd410cd759e8e4b5fd9b0e5881049a063d0fc0
Mic-Tartaglia <mic.tartaglia98@gmail.com>
2020-06-14 16:35:11 +0200

    Week4

---
+++ Week4/es4.3/solid/MolDyn_NVE.h
@@ -0,0 +1,71 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+//parameters, observables
+const int m_props=4;
+int n_props;
+int iv,ik,it,ie;
+double stima_pot, stima_kin, stima_etot, stima_temp;
+
+//option to restart
+bool restart;
+
+// averages
+double acc,att;
+double Epot_ave;
+double Ekin_ave;
+double Temp_ave;
+double Etot_ave;
+
+//configuration
+const int m_part=108;
+double x[m_part],y[m_part],z[m_part],xold[m_part],yold[m_part],zold[m_part];
+double vx[m_part],vy[m_part],vz[m_part];
+
+// thermodynamical state
+int npart;
+double energy,temp,vol,rho,box,rcut;
+double T_desired;
+
+// simulation
+int nstep, iprint, seed;
+double delta;
+
+//error calculation
+int nblocks;
+int blk_len;
+double ** ave;
+
+//functions
+void Input(void);
+void Move(void);
+void ConfFinal(void);
+void ConfXYZ(int);
+void Measure(void);
+double Force(int, int);
+double Pbc(double);
+void Sum();
+void Average(int);
+void Print_Ave();
+double StdDev(double,double,int);
+
+
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+
+
+
